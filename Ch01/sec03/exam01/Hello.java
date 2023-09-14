3611bf836454910c22682dbb13f3e146eb57ce6e
andan1 <139194353+andan1@users.noreply.github.com>
2023-09-14 09:51:58 +0900

    ch01

---
+++ Ch01/sec03/exam01/Hello.java
@@ -0,0 +1,12 @@
+package Ch01.sec03.exam01;
+// 주석 사용하기
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+
+}
+// // : 라인 주석
+// /* ~ */ : 범위 주석
+// /** ~ */ : 도큐먼트 주석
